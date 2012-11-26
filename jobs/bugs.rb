2dfba8b328308074cab5ee1e154b37fb17ab8a6e
Joe Mahoney <jmahoney@iterative.co.nz>
2012-11-26 13:58:48 +1300

    two different bug counts
    showing all open bugs and a separate count of new urgent bugs

--- jobs/bugs.rb
+++ jobs/bugs.rb
@@ -1,10 +1,19 @@
-current_count = 0
-
+current_open_bug_count = 0
+current_new_urgent_bug_count = 0
 SCHEDULER.every '10s' do
-  last_count = current_count
   
-  current_count = RedmineWeary::Issue.count
   
-  send_event('bugs', { current: current_count, last: last_count })
+  last_open_bug_count = current_open_bug_count
+  current_open_bug_count = RedmineWeary::Issue.count(:tracker_id => BUG_TRACKER,
+                                                     :status_id => OPEN_STATUS)
+  
+  last_new_urgent_bug_count = current_new_urgent_bug_count
+  current_new_urgent_bug_count = RedmineWeary::Issue.count(:tracker_id => BUG_TRACKER,
+                                                           :status_id => NEW_STATUS,
+                                                           :priority_id => URGENT_PRIORITY)
+  
+                                                       
+  send_event('open-bugs', { current: current_open_bug_count, last: last_open_bug_count })
+  send_event('new-urgent-bugs', { current: current_new_urgent_bug_count, last: last_new_urgent_bug_count })
   
 end
